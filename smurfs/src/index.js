298d269ff64f9baa259efde026a3dcad89903224
Kim Duclos <kimduclos@gmail.com>
2019-03-08 09:26:23 -0700

    project set up built

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import rootReducer from './reducers';
+import App from './components/App';
+import './reducers/index';  /* You need some sort of reducer */ 
 
-const store = createStore(
-  () => {}, // this is the most basic reducer. A function that returns and object. Replace it.
-  applyMiddleware(/* be sure to throw in the proper middlewares here*/)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers ( // this is the most basic reducer. A function that returns and object. Replace it.
+  applyMiddleware(/* be sure to throw in the proper middlewares here*/thunk, logger))
 );
 
 ReactDOM.render(
